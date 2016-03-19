46a0e7e244c0d8656e87be461b048189c5868413
Rytis <rytis.grincevicius@gmail.com>
2016-03-19 17:34:07 +0200

    Undefined variable for servers on XAMPP using Mac OS.

---
+++ Request.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @author Rytis Grincevicius <rytis@kiberzauras.com>
+ * @link http://www.github.com/kiberzauras/laravel.multilanguage
+ * @version 2.0.2
+ * @license MIT
+ */
+namespace Kiberzauras\MultiLanguage;
+
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
+use Illuminate\Http\Request as LaravelRequest;
+
+class Request extends LaravelRequest
+{
+    /** There we will check if first segment of request uri is one our supported
+     * languages, then we will set it to locale and remove from request, this way
+     * there will be no need to configure our routes.php file for any of languages.
+     * @return LaravelRequest
+     *
+     * @todo If request doesn't have language, it would be great to check browser language or location.
+     */
+    public static function capture()
+    {
+        $config_url = config_path() . DIRECTORY_SEPARATOR . 'multilanguage.json';
+        if (file_exists($config_url)) {
+            $params = json_decode(file_get_contents($config_url));
+
+            if (!empty($params->enabled)) {
+
+                if (array_key_exists('HTTP_X_ORIGINAL_URL', $_SERVER))
+                    self::parseServerVars('HTTP_X_ORIGINAL_URL', $params);
+
+                elseif (array_key_exists('HTTP_X_REWRITE_URL', $_SERVER))
+                    self::parseServerVars('HTTP_X_REWRITE_URL', $params);
+
+                elseif (!empty($_SERVER['UNENCODED_URL']) && $_SERVER['IIS_WasUrlRewritten'] == 1)
+                    self::parseServerVars('UNENCODED_URL', $params);
+
+                elseif ($_SERVER['REQUEST_URI'])
+                    self::parseServerVars('REQUEST_URI', $params);
+
+                elseif ($_SERVER['ORIG_PATH_INFO'])
+                    self::parseServerVars('ORIG_PATH_INFO', $params);
+
+            }
+            defined('Language') || define('Language', !empty($params->default) ? $params->default : 'en');
+        }
+
+        static::enableHttpMethodParameterOverride();
+        return static::createFromBase(SymfonyRequest::createFromGlobals());
+    }
+
+    /**
+     * @param $var
+     * @param $params
+     */
+    protected static function parseServerVars($var, $params)
+    {
+        $uri = trim($_SERVER[$var], '/');
+        $lang = strstr($uri, '/', true);
+        if (in_array($lang, $params->enabled)) {
+            // for accessing /en/page/page
+            $_SERVER[$var] = strstr($uri, '/');
+            define('Language', $lang);
+        } elseif (in_array($uri, $params->enabled)) {
+            // for accessing /, /en, and /en/ pages
+            $_SERVER[$var] = '/';
+            define('Language', $uri);
+        }
+    }
+}
